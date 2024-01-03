dea1dd2973b41fd9d07252098d4bc23bdcd23230
chett23 <chesterehansen@gmail.com>
2024-01-03 15:56:12 -0700

    added useWindowWidth hook, WIP, added ternary to remove labels from anvbar when less than 870px in width

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,24 +5,34 @@ import { UserIcon } from "@heroicons/react/24/solid";
 import { MagnifyingGlassIcon, BellIcon } from "@heroicons/react/24/outline";
 
 import { useScrollPosition } from "../utils/useScrollPosition";
+import { useWindowDimensions } from "../utils/useWindowDimensions";
 
 export const Navbar = () => {
   const scrollPosition = useScrollPosition();
+  const [width] = useWindowDimensions();
 
   return (
     <header
-      className={`z-10 flex w-full flex-row justify-between md:fixed bg-gradient-to-b from-background-950 ${
+      className={`z-10 flex w-full flex-row justify-between bg-gradient-to-b from-background-950 md:fixed ${
         scrollPosition > 42 && " bg-background-950"
       } transition duration-300`}
     >
       <div className={"flex flex-row items-center space-x-6 p-4"}>
         <img src={netflix_logo} className={"w-24"} alt="netflix logo" />
-        <p className="text-xs text-font-50 md:text-sm ">Home</p>
-        <p className="text-xs text-font-50 md:text-sm ">TV Shows</p>
-        <p className="text-xs text-font-50 md:text-sm ">Movies</p>
-        <p className="text-xs text-font-50 md:text-sm ">New & popular</p>
-        <p className="text-xs text-font-50 md:text-sm ">My List</p>
-        <p className="text-xs text-font-50 md:text-sm ">Browse by Language</p>
+        {width <= 870 ? (
+          <></>
+        ) : (
+          <>
+            <p className="text-xs text-font-50 md:text-sm ">Home</p>
+            <p className="text-xs text-font-50 md:text-sm ">TV Shows</p>
+            <p className="text-xs text-font-50 md:text-sm ">Movies</p>
+            <p className="text-xs text-font-50 md:text-sm ">New & popular</p>
+            <p className="text-xs text-font-50 md:text-sm ">My List</p>
+            <p className="text-xs text-font-50 md:text-sm ">
+              Browse by Language
+            </p>
+          </>
+        )}
       </div>
       <div className={"flex flex-row items-center space-x-4 p-4"}>
         <MagnifyingGlassIcon className="h-6 w-6 text-font-50" />
